e02282b3d1be145f8808766c6e8565cb6af90633
TakeAsh <take@TakeAsh.net>
2022-05-04 15:50:37 +0900

    add Negate-Movie.ps1

---
+++ Negate-Movie.ps1
@@ -0,0 +1,17 @@
+﻿<#
+  .SYNOPSIS
+    Negate the movie file.
+    Negate-Movie.ps1 <filename> [<filename>...]
+
+  .DESCRIPTION
+    To use this script, create a shortcut to 'powershell.exe -File "fullpath\to\Negate-Movie.ps1"' in '%AppData%\Microsoft\Windows\SendTo'.
+#>
+
+
+$args | ForEach-Object {
+  $path = $_
+  $dir = [System.IO.Path]::GetDirectoryName($path)
+  $filename = [System.IO.Path]::GetFileNameWithoutExtension($path)
+  $ext = [System.IO.Path]::GetExtension($path)
+  ffmpeg.exe -y -i "$path" -vf "lutyuv='y=negval:u=negval:v=negval'" -bsf:a aac_adtstoasc -acodec copy -movflags faststart "${dir}/${filename}n${ext}"
+}
