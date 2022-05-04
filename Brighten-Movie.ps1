d911e83f24408a68bbe38f0b82243c3d15e3058b
TakeAsh <take@TakeAsh.net>
2022-05-04 15:50:20 +0900

    add Brighten-Movie.ps1

---
+++ Brighten-Movie.ps1
@@ -0,0 +1,16 @@
+﻿<#
+  .SYNOPSIS
+    Blighten the movie file with gamma value.
+    Brighten-Movie.ps1 <filename> [<filename>...]
+
+  .DESCRIPTION
+    To use this script, create a shortcut to 'powershell.exe -File "fullpath\to\Brighten-Movie.ps1"' in '%AppData%\Microsoft\Windows\SendTo'.
+#>
+
+$args | ForEach-Object {
+  $path = $_
+  $dir = [System.IO.Path]::GetDirectoryName($path)
+  $filename = [System.IO.Path]::GetFileNameWithoutExtension($path)
+  $ext = [System.IO.Path]::GetExtension($path)
+  ffmpeg.exe -y -i "$path" -vf "lutyuv='y=gammaval(0.6)'" -bsf:a aac_adtstoasc -acodec copy -movflags faststart "${dir}/${filename}g${ext}"
+}
